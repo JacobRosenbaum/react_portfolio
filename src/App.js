9463090ddda09061501481d0283e1de6d350871e
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-02-19 15:46:50 -0600

    added contact page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import About from "./components/pages/About/js/About.js";
+import Contact from "./components/pages/Contact/js/Contact";
 
 
 
 function App() {
   return (
+    <div>
     <About />
+    <Contact />
+    </div>
   );
 }
 
---
+++ src/components/pages/Contact/js/Contact.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "../css/contact.css";
+
+function Contact() {
+  return (
+    <div class="container">
+    <div class="slideInLeft">
+        <div class="jumbotron">
+            <h1 class="display-4">Contact </h1>
+            <hr class="my-4" />
+            <div id="information">
+                <div id="contactInfo">
+                    <div id="email">
+                        <i className="fas fa-envelope"></i> jacobrosenbaum95@gmail.com
+                    </div>
+                    <br />
+                    <div id="github">
+                        <i className="fab fa-github "></i> <a href="https://github.com/JacobRosenbaum " style= {{color:"#777777"}} target="blank"> Github</a>
+
+                    </div>
+                    <br />
+                    <div id="linkedIn">
+                        <i className="fab fa-linkedin "></i><a href="https://www.linkedin.com/in/jacobrosenbaum/ " style= {{color:"#777777"}} target="blank" > LinkedIn</a>
+
+                    </div>
+                    <br />
+                    <div id="resume">
+                        <i className="fas fa-graduation-cap "></i><a href="https://docs.google.com/document/d/1dU1evbdeRogSlUbvjAox5Ar5ZIFT-qYs0a5nwY5wMMs/edit?usp=sharing" style= {{color:"#777777"}} target="blank" > Resume</a>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+     
+  );
+}
+
+export default Contact;
