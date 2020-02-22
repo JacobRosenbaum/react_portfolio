f4c05877d0b4b3d7c06d013d080bf37fbe22fa39
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-02-22 14:59:06 -0600

    created build folder and domain name

---
+++ build/precache-manifest.420fddcfa7c7e648bd91035547e9b523.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1a0e85480daf77a803a9fc9e93eff8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c8fa1b2b5e17f4bcdc9",
+    "url": "/static/css/main.e5a6cc45.chunk.css"
+  },
+  {
+    "revision": "493219546fc60ff72dff",
+    "url": "/static/js/2.d7b72919.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.d7b72919.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c8fa1b2b5e17f4bcdc9",
+    "url": "/static/js/main.98ef51d6.chunk.js"
+  },
+  {
+    "revision": "2a6b59439d6da1d5e2cd",
+    "url": "/static/js/runtime-main.ebc823aa.js"
+  },
+  {
+    "revision": "519a75502b453c45f0a34a8444aa6714",
+    "url": "/static/media/books.519a7550.png"
+  },
+  {
+    "revision": "446b6da141d4496526c027354b5d3e0b",
+    "url": "/static/media/burger2.446b6da1.png"
+  },
+  {
+    "revision": "3ab265fe310bf7213d5e666f17a403e3",
+    "url": "/static/media/concrete.3ab265fe.png"
+  },
+  {
+    "revision": "19b3c8bca6fdc32aa7fc72333143931a",
+    "url": "/static/media/employee_directory.19b3c8bc.png"
+  },
+  {
+    "revision": "1f801a19290fc94312b9d36418cad51d",
+    "url": "/static/media/fieldcopy.png.1f801a19.jpg"
+  },
+  {
+    "revision": "d38fcc4111f63c08cff11d18a4fa7181",
+    "url": "/static/media/headshotJacob.d38fcc41.jpg"
+  },
+  {
+    "revision": "7bc646aa2a568f083c67930e88a20612",
+    "url": "/static/media/mountain.7bc646aa.jpg"
+  }
+]);
